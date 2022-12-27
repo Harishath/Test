b76916defa9a0c7417a67e56afac1bf7c952f7e9
91911 <91911@HARISH>
2022-12-27 15:13:28 +0530

    build 101

---
+++ Krishana/src/Ramachari/Demo.java
@@ -0,0 +1,14 @@
+package Ramachari;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+public class Demo {
+
+	public static void main(String[] args) {
+	System.setProperty("webdriver.gecko.driver","./Punitha/geckodriver.exe");
+	WebDriver driver=new FirefoxDriver();
+
+	}
+
+}
